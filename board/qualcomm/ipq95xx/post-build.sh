31039566d6f9fd4d7aa49f1805012ecd5a5149df
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2025-12-01 20:20:54 -0600

    ipq95xx: move initramfs load address to 0x52000000
    With the kernel load address at 0x41000000, the uncompressed image
    cannot exceed 38 MB. If it does, the bootloader overwrites itself and
    crashes. At 0x42000000, the headromm is likely smaller. With all the
    firmware images, this can be easily exceeded,
    
    It seems that moving things to 0x52000000 solves this problem.

--- board/qualcomm/ipq95xx/post-build.sh
+++ board/qualcomm/ipq95xx/post-build.sh
@@ -7,7 +7,7 @@ MKIMAGE=$HOST_DIR/bin/mkimage
 # Generate the FIT image
 $MKIMAGE -f auto \
         -A arm64 \
-        -a 0x42000000 \
+        -a 0x52000000 \
         -d "$BINARIES_DIR/Image.gz" \
         -b "$BINARIES_DIR/ipq9554-be550-lnx.dtb" \
         "$BINARIES_DIR/uImage.itb"
